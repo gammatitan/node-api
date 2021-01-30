0af1d7ef64564caa1f0f9dc0893b6b95911781dd
Jake Brown <mynameisjakebrown@gmail.com>
2021-01-30 23:58:46 +0000

    Add endpoint to trigger important actions on a users account

--- src/api/users.ts
+++ src/api/users.ts
@@ -6,7 +6,9 @@ import User from '../entity/user';
 import ApiError from '../services/api-error';
 import UserFactory from '../services/factory/user.factory';
 import PasswordClient from '../services/password-client';
+import PasswordResetManager from '../services/password-reset-manager';
 import RouterFunctions from '../services/router-functions';
+import UserManager from '../services/user-manager';
 import createAdminSchema from '../validation/create-admin.validation';
 import updatePasswordSchema from '../validation/update-password.schema';
 import updatePersonalAccountInfoSchema from '../validation/update-personal-account-info.schema';
@@ -110,6 +112,40 @@ const user = (app: Router) => {
             res.json(data);
         } catch {
             next(ApiError.badRequest());
+        }
+    });
+
+    /**
+     * Trigger an important action for a specific user
+     */
+    route.put('/:id/action/:type', isAdmin, async (req: Request, res: Response, next: NextFunction) => {
+        const userRepo = getManager().getRepository(User);
+        const user = await userRepo.findOne(req.params.id);
+
+        if (!user) {
+            next(ApiError.badRequest('User not found'));
+
+            return;
+        }
+
+        try {
+            switch (req.params.type) {
+                case 'activate':
+                    await UserManager.activate(user, req.user);
+                    break;
+                case 'deactivate':
+                    await UserManager.deactivate(user, req.user);
+                    break;
+                case 'reset-password':
+                    await PasswordResetManager.request(user.emailAddress);
+                    break;
+                default:
+                // Do nothing.
+            }
+
+            res.json({});
+        } catch (e) {
+            next(ApiError.badRequest(e));
         }
     });
 
